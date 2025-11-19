ab8f9d8604977ee26227d968403c93b5a364d4a5
joze-liburn <joze@lightburnsoftware.com>
2025-11-19 13:25:41 +0100

    Postgres support - parameters named.

--- external_ack_queue.go
+++ external_ack_queue.go
@@ -14,20 +14,20 @@ func NewExternalAckQueue(db *sql.DB, ackOpts AckOpts, opts ...QueueOptions) (*Ac
 	qo.Apply(opts...)
 
 	ack := map[DequeueAction]string{
-		DequeueMark:   Flavour("call gopq_ack_store(?, ?)", qo.CallConvention),
-		DequeueDelete: Flavour("call gopq_ack_delete(?, ?)", qo.CallConvention),
+		DequeueMark:   Flavour("call gopq_ack_store(:p_id, :now)", qo.CallConvention),
+		DequeueDelete: Flavour("call gopq_ack_delete(:p_id, :now)", qo.CallConvention),
 	}
 	bq := BaseQueries{
-		enqueue:    Flavour("call gopq_push_ack(?)", qo.CallConvention),
-		tryDequeue: Flavour("call gopq_pop_ack(?, ?)", qo.CallConvention),
-		len:        Flavour("call gopq_len_ack(?)", qo.CallConvention),
+		enqueue:    Flavour("call gopq_push_ack(:it)", qo.CallConvention),
+		tryDequeue: Flavour("call gopq_pop_ack(:now, :deadline)", qo.CallConvention),
+		len:        Flavour("call gopq_len_ack(:now)", qo.CallConvention),
 	}
 	aq := AckQueries{
 		ackUtilsQueries: ackUtilsQueries{
-			details:  Flavour("call gopq_selectItemDetails(?)", qo.CallConvention),
-			delete:   Flavour("call gopq_deleteItem(?)", qo.CallConvention),
-			forRetry: Flavour("call gopq_updateForRetry(?, ?)", qo.CallConvention),
-			expire:   Flavour("call gopq_expireAckDeadline(?, ?)", qo.CallConvention),
+			details:  Flavour("call gopq_selectItemDetails(:p_id)", qo.CallConvention),
+			delete:   Flavour("call gopq_deleteItem(:p_id)", qo.CallConvention),
+			forRetry: Flavour("call gopq_updateForRetry(:p_deadline, :p_id)", qo.CallConvention),
+			expire:   Flavour("call gopq_expireAckDeadline(:p_deadline, :p_id)", qo.CallConvention),
 		},
 		ack: Flavour(ack[qo.DequeueAction], qo.CallConvention),
 	}
